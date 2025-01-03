837d0a1b2a357a4310cc702d0a14824a28ce26a7
AndHak <andresfg13789@gmail.com>
2025-01-03 03:23:27 -0500

    js

---
+++ Desarrollo web/Proyectos/Tienda Restaurante/script.js
@@ -0,0 +1,45 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".mySwiper-1 .swiper-button-next",
+        prevEl: ".mySwiper-1 .swiper-button-prev",
+    }
+});
+
+var swiper2s = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".mySwiper-2 .swiper-button-next",
+        prevEl: ".mySwiper-2 .swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
+    }
+});
+
+let tabInputs = document.querySelectorAll(".tabInput");
+
+tabInputs.forEach(function(input) {
+    input.addEventListener('change', function() {
+        let id = input.value;
+        let thisSwiper = document.getElementById("swiper" + id);
+        thisSwiper.swiper2.update();
+    });
+});
